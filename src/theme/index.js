9ae9ce8defff6bb23f36d4d1aa2ab3f6bb227ed2
DiegoMfer <diegomartinfnz@gmail.com>
2025-02-17 19:37:46 +0100

    initial app

---
+++ src/theme/index.js
@@ -0,0 +1,13 @@
+import { colors } from "./colors.js";
+import { space } from "./spacing.js";
+import { sizes } from "./sizes.js";
+import { fonts, fontWeights, fontSizes } from "./fonts";
+
+export const theme = {
+  colors,
+  fonts,
+  fontSizes,
+  fontWeights,
+  sizes,
+  space,
+};
